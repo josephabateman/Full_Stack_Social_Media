ab6d16b0bb9066fcd6b97fcfa8c79847cdc3fa1c
josephabateman <josephabateman@yahoo.co.uk>
2020-12-29 17:36:20 +0000

    comments and time posted work well.

--- backend/app.js
+++ backend/app.js
@@ -61,11 +61,9 @@ app.post('/signup', (req, res) => {
                         const client = await pool.connect()
                         await client.query(`INSERT INTO users (user_email, password, user_id) VALUES ('${email}', '${hash}', '${postIdRandStr}');`)
                         client.release()
-                        console.log('signed up')
                         res.status(200).json({
                             message: 'email and password stored successfully'
                         })
-                        await pool.end()
                     } catch (error) {
                         console.log(`Error when adding user to database: '${error.detail}' - Full error: ${error}`)
                         res.status(409).json({
@@ -131,7 +129,69 @@ app.get('/', (req, res) => {
             const client = await pool.connect()
             const databaseQuery = await client.query("SELECT * FROM posts");
             client.release()
-            res.send(databaseQuery.rows)
+            // console.log(databaseQuery.rows)
+            const sendArray = []
+            // console.log(databaseQuery.rows)
+
+            for (post of databaseQuery.rows) {
+
+                //here is where i convert milliseconds to time elapsed since posted
+                let milliseconds = parseInt(post.time_stamp)
+                let timeNow = Date.now()
+                let difference = timeNow - milliseconds
+                let timeElapsed = new Date(difference)
+
+                //time calculations
+                const seconds = timeElapsed / 1000
+                const minutes = seconds / 60
+                const hours = minutes / 60
+                const days = hours / 24
+                const weeks = days / 30
+                const years = days / 365
+
+                //use a switch statement
+                let measurement
+                if (minutes <= 1) {
+                    measurement = `Less than a minute ago`
+                } else if (minutes > 1 && minutes < 60) {
+                    measurement = `${Math.round(minutes)} minutes ago`
+                } else if (minutes >= 60 && minutes < 120) {
+                    measurement = `about an hour ago`
+                } else if (minutes >= 120) {
+                    measurement = `${Math.trunc(hours)} hours ago`
+                } else if (hours > 24) {
+                    measurement = `yesterday`
+                } else if (days > 2) {
+                    measurement = `${Math.trunc(days)} days ago`
+                } else if (weeks > 1) {
+                    measurement = `${Math.trunc(weeks)} weeks ago`
+                } else if (years > 1) {
+                    measurement = `${Math.trunc(years)} years ago`
+                }
+
+                if (post.testarray !== null) {
+                    post.testarray.forEach(function(comment, i) {
+                        if (typeof comment == "string")
+                            post.testarray[i] = JSON.parse(comment); });
+
+                    // a.forEach(function(item, i) { if (item == 3452) a[i] = 1010; });
+                }
+
+                // i send the get object here
+                const getObject = {
+                    caption: post.caption,
+                    file_upload: post.file_upload,
+                    user_id: post.user_id,
+                    post_id: post.post_id,
+                    time_stamp: post.time_stamp,
+                    comments: post.testarray,
+                    convertedTime: measurement
+                }
+                sendArray.push(getObject)
+                 // console.log(getObject)
+            }
+            // console.log(...databaseQuery.rows)
+            res.send(sendArray)
         }
         displayPosts();
     } catch (error) {
@@ -150,9 +210,11 @@ app.post('/posts', authenticateToken, (req, res) => {
             try {
                 const imageUrl = req.protocol + '://' + req.get('host') + '/uploads/' + req.file.filename
                 const postIdRandStr = randomstring.generate(16);
+                const timestamp = new Date()
+                const timestampMilliseconds = timestamp.getTime()
 
                 const client = await pool.connect()
-                const databaseQuery = await client.query(`INSERT INTO posts (caption, user_id, file_upload, post_id) VALUES ('${req.body.caption}', '${req.body.userId}', '${imageUrl}', '${postIdRandStr}') RETURNING post_id;`)
+                const databaseQuery = await client.query(`INSERT INTO posts (caption, user_id, file_upload, post_id, time_stamp) VALUES ('${req.body.caption}', '${req.body.userId}', '${imageUrl}', '${postIdRandStr}', '${timestampMilliseconds}') RETURNING post_id;`)
                 client.release()
 
                 const postId = databaseQuery.rows[0].post_id
@@ -239,4 +301,121 @@ app.delete('/posts', authenticateToken, (req, res) => {
         }
     }
     deletePost()
+})
+
+//put and delete share most of the same code - refactor as a function
+app.delete('/deleteUser', authenticateToken, (req, res) => {
+    async function deleteUser() {
+        try {
+
+            const decodedUserId = res.locals.testing
+            const client = await pool.connect()
+            const result = await client.query(`SELECT * FROM users WHERE user_id = '${decodedUserId}'`)
+            const databaseId = result.rows[0].user_id
+            // console.log(decodedUserId)
+            // console.log(databaseId)
+
+            if (databaseId === decodedUserId) {
+                const client = await pool.connect()
+                //use a join instead of three queries!
+                const databaseQuery = await client.query(`SELECT file_upload FROM posts WHERE user_id = '${decodedUserId}';`)
+                await client.query(`DELETE FROM posts WHERE user_id = '${decodedUserId}';`)
+                await client.query(`DELETE FROM users WHERE user_id = '${decodedUserId}'`)
+                client.release()
+                res.json({
+                    message: 'user account deleted successfully'
+                })
+
+                //this gets all the filenames from database and splits off the filename, pushing to array for deletion
+                let fileUploadsArray = []
+                databaseQuery.rows.forEach(row => fileUploadsArray.push(row.file_upload.split('/uploads/')[1]))
+
+                fs.readdir('uploads/', (err, files) => {
+                    if (err) throw err;
+                    for (const file of files) {
+                        //list array matches here
+                        if(fileUploadsArray.includes(file)) {
+                            fs.unlink(path.join('uploads/', file), err => {
+                                if (err) throw err;
+                            });
+                        }
+                    }
+                });
+            } else {
+                res.status(403).json({
+                    error: 'You are not authorised to delete this post'
+                })
+            }
+        } catch (error) {
+            console.log(error)
+        }
+    }
+    deleteUser()
+})
+
+app.post('/addComment', authenticateToken, (req, res) => {
+    async function addComment() {
+        try {
+            const decodedUserId = res.locals.testing
+            const commentIdRandStr = randomstring.generate(16)
+            // console.log(req.body.post_Id)
+
+            const object = {
+                commentId: commentIdRandStr,
+                userId: decodedUserId,
+                comment: req.body.comment,
+                postId: req.body.post_Id
+            }
+            const objectAsString = JSON.stringify(object)
+
+            const client = await pool.connect()
+            await client.query(`UPDATE posts SET testarray = array_append(testarray, '${objectAsString}') WHERE post_id = '${req.body.post_Id}';`)
+            const databaseQuery = await client.query(`SELECT * FROM posts WHERE post_id = '${req.body.post_Id}';`)
+            client.release()
+            // console.log(databaseQuery.rows)
+
+            // const postId = databaseQuery.rows[0].post_id
+            res.json({
+                // postId: postId,
+                status: 'comment added sucessfully'
+            })
+        } catch (error) {
+            console.log(error)
+            res.status(404).json({error: 'did you fill out all fields and upload a gif?'})
+        }
+    }
+addComment()
+})
+
+app.delete('/deleteComment', authenticateToken, (req, res) => {
+    async function deleteComment() {
+        try {
+            //security implementation
+            const decodedUserId = res.locals.testing
+
+            const commentToDelete = {
+                commentId: req.body.commentId,
+                userId: req.body.userId,
+                comment: req.body.comment,
+                postId: req.body.postId
+            }
+
+            // console.log(commentToDelete)
+
+            const commentToDeleteString = JSON.stringify(commentToDelete)
+            // console.log(commentToDeleteString)
+
+            const client = await pool.connect()
+            await client.query(`UPDATE posts SET testarray = array_remove(testarray, '${commentToDeleteString}');`)
+            client.release()
+
+            res.json({
+                message: 'comment deleted sucessfully'
+            })
+        } catch (error) {
+            console.log(error)
+            res.status(404).json({error: 'did you fill out all fields and upload a gif?'})
+        }
+    }
+    deleteComment()
 })
